58f86c3de7965a7d8cf7632dd84c4b2215fd0c23
Normanity <norm0066@algonquinlive.com>
2018-03-08 18:23:56 -0500

    CST2335 Final Project
    Added simple main menu and placeholders

--- app/src/main/java/com/example/wn/cst2335_final_project/MainActivity.java
+++ app/src/main/java/com/example/wn/cst2335_final_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.wn.cst2335_final_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button startBtn = (Button) findViewById(R.id.startButton);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent mainMenu = new Intent(MainActivity.this, MainMenu.class);
+                startActivity(mainMenu);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/wn/cst2335_final_project/MainMenu.java
@@ -0,0 +1,53 @@
+package com.example.wn.cst2335_final_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+/*
+        Button startQuiz = (Button) findViewById(R.id.startQuizApp);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent quizMain = new Intent (MainMenu.this, QuizMain.class);
+                startActivity(quizMain);
+            }
+        });
+
+        Button startPatient = (Button) findViewById(R.id.startPatientForm);
+        startPatient.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent patientMain = new Intent (MainMenu.this, PatientMain.class);
+                startActivity(patientMain);
+            }
+        });
+
+
+        Button startMovie = (Button) findViewById(R.id.startMovie);
+        startMovie.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent startMovie = new Intent (MainMenu.this, MovieMain.class);
+                startActivity(startMovie);
+            }
+        });
+
+
+        Button startOCTranspo = (Button) findViewById(R.id.startOCTranspo);
+        startOCTranspo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent OCTMain = new Intent (MainMenu.this, OCTranspoMain.class);
+                startActivity(OCTMain);
+            }
+        });
+
+*/
+
+
+
+
+    }
+}
